ee492737bc43bc8365ff868e1e918cb79f256148
Jason <jlavery828@gmail.com>
2020-07-12 13:40:05 -0400

    fixed up login and navbar

--- blog/settings.py
+++ blog/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+LOGIN_REDIRECT_URL = '/'
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
