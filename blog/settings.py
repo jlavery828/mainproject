db7973929a86f213aa1a860d53a930d51938b01e
Jason <jlavery828@gmail.com>
2020-08-09 11:40:06 -0400

    added websitenear.me to allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '104.131.12.98']
+ALLOWED_HOSTS = ['127.0.0.1', '104.131.12.98',
+                 'www.websitenear.me', 'websitenear.me']
 
 
 # Application definition
